0e966690c2098b5a283758c2cf4271caef846a79
ikerbede <iker.bedecarrax@gmail.com>
2023-06-24 00:32:57 +0200

    fix(pizza-details): move the html templatea

--- pizza-details.component.ts
+++ pizza-details.component.ts
@@ -1,16 +1,54 @@
+const template = `
+<template id="pizza-details-template">
+  <style>
+    .pizza-details {
+      border: 1px solid black;
+      border-radius: 4px;
+
+      width: 150px;
+      padding: 16px;
+    }
+    .pizza-details-header {
+      margin-bottom: 16px;
+      padding-bottom: 16px;
+      border-bottom: 1px solid black;
+      display: flex;
+      justify-content: space-between;
+    }
+    .pizza-details-header > h3,
+    h4 {
+      margin: 0;
+    }
+    .pizza-details-footer {
+      display: flex;
+      justify-content: flex-end;
+    }
+  </style>
+  <article class="pizza-details">
+    <header class="pizza-details-header">
+      <h3><slot name="pizza-name"></slot></h3>
+      <h3><slot name="pizza-price"></slot></h3>
+    </header>
+    <h4>Ingrédients :</h4>
+    <slot name="pizza-ingredients"></slot>
+    <footer class="pizza-details-footer">
+      <button type="button" id="button-buy">BUY</button>
+    </footer>
+  </article>
+</template>
+`;
+
 export class PizzaDetailsComponent extends HTMLElement {
   constructor() {
     super();
-    fetch('./pizza-details.component.html')
-      .then((response: Response) => response.text())
-      .then((template: string) => {
-        console.log('template: ', template);
-        document.body.insertAdjacentHTML('afterbegin', template);
-        const templateElt = document.getElementById(
-          'pizza-details-template'
-        ) as HTMLTemplateElement;
-        const shadowRoot = this.attachShadow({ mode: 'open' });
-        return shadowRoot.appendChild(templateElt.content.cloneNode(true));
-      });
+
+    const parser = new DOMParser();
+    const dom = parser.parseFromString(template, 'text/html');
+
+    const templateElt = dom.getElementById(
+      'pizza-details-template'
+    ) as HTMLTemplateElement;
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.appendChild(templateElt.content.cloneNode(true));
   }
 }
